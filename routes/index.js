75f943de02b09116a2b3751dce9ade78e6c53bea
chan-kim <chan.ckeem@gmail.com>
2022-03-16 15:06:05 +0900

    C 2022.3.16.3:05(PM)
    setting routing

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("home");
+});
+
+router.get("/timer", (req, res) => {
+  res.render("timer");
+});
+
+router.get("/pomodoro", (req, res) => {
+  res.render("pomodoro");
+});
+
+router.get("/custom", (req, res) => {
+  res.render("custom");
 });
 
 module.exports = router;
