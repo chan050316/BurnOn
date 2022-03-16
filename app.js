3d7e999662561f7f73dc6281ef91a299fccced71
chan-kim <chan.ckeem@gmail.com>
2022-03-16 14:26:18 +0900

    C 2022.3.16.2:26(PM)
    create routing file

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
 
 const PORTNUM = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use("/", indexRouter);
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.listen(PORTNUM, () => {
   console.log(PORTNUM + "에서 대기중");
