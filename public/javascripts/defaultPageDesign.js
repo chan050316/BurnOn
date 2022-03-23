95b98b825a4823540b24ac3efb29030b18c7fff8
chan-kim <chan.ckeem@gmail.com>
2022-03-23 12:34:06 +0900

    C 2022.3.23.12:33(PM)
    create side bar

--- public/javascripts/defaultPageDesign.js
+++ public/javascripts/defaultPageDesign.js
@@ -1,5 +1,8 @@
+const bodyBoxSideMenu = document.querySelector("#bodyBox-sideMenuJS");
+const closeSideBar = document.querySelector("#closeSideBarJS");
 const btnPlay = document.querySelector("#btn-playJS");
 const iconPlay = document.querySelector("#icon-playJS");
+const btnMenu = document.querySelectorAll(".btn-menu");
 
 function playingMusic() {
   if (iconPlay.className === "material-icons icon-pause icon-play") {
@@ -10,4 +13,30 @@ function playingMusic() {
   iconPlay.classList.toggle("icon-play");
 }
 
+function sideBar__open() {
+  bodyBoxSideMenu.classList.add("sideBar");
+  closeSideBar.classList.remove("hidden");
+  btnMenu.forEach(div => {
+    div.classList.add("sideBar__btn-menu");
+    div.classList.remove("checked");
+  });
+  this.classList.toggle("checked");
+  if (this.childNodes[0].className.indexOf("icon-hamburger") === 0) {
+    console.log(1);
+  } else if (this.childNodes[0].className.indexOf("icon-music") === 0) {
+    console.log(2);
+  } else if (this.childNodes[0].className.indexOf("icon-volume") === 0) {
+    console.log(3);
+  }
+}
+
+function sideBar__close() {
+  bodyBoxSideMenu.classList.remove("sideBar");
+  closeSideBar.classList.add("hidden");
+}
+
 btnPlay.addEventListener("click", playingMusic);
+btnMenu.forEach(btn => {
+  btn.addEventListener("click", sideBar__open);
+});
+closeSideBar.addEventListener("click", sideBar__close);
