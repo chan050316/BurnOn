18c12abb3a4a1e50bdc0418496bbb0a74e5009df
chan-kim <chan.ckeem@gmail.com>
2022-04-07 23:36:34 +0900

    C 2022.4.7.11:36(PM)
    build timer mechanism

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -2,19 +2,21 @@ const timer = document.querySelector("#timerJS");
 const timerNums = timer.querySelectorAll(".timerNumJS");
 const timerTimeBoxs = timer.querySelectorAll(".timerTimeBoxJS");
 const timerHours = timer.querySelectorAll(".timerHourJS");
-const timerMinute = timer.querySelectorAll(".timerMinuteJS");
+const timerMinutes = timer.querySelectorAll(".timerMinuteJS");
 const timerStart = document.querySelector("#timerStart");
 const timerStop = document.querySelector("#timerStop");
 const timerEnd = document.querySelector("#timerEnd");
+
+const timerNumValue = [];
+let countHourNum = 0;
+let countMinuteNum = 0;
 
 function inputMaxlength() {
   if (this.value.length > 2) {
     this.value = this.value.substr(0, 2);
   }
 }
-
 function timerStarting() {
-  const timerNumValue = [];
   timerStart.style.display = "none";
   timerStop.style.display = "flex";
   timerNums.forEach(El => {
@@ -26,15 +28,52 @@ function timerStarting() {
   });
   // 입력된게 숫자가 아니라면 알림 띄우기
   timerHours.forEach(El => {
-    El.style.transform = `translateY(-${timerNumValue[0] * 40}px)`;
+    El.style.transform = `translateY(-${
+      timerNumValue[0] * 40 - countHourNum * 40
+    }px)`;
   });
-  timerMinute.forEach(El => {
-    El.style.transform = `translateY(-${timerNumValue[1] * 40}px)`;
+  timerMinutes.forEach(El => {
+    El.style.transform = `translateY(-${
+      timerNumValue[1] * 40 - countMinuteNum * 40
+    }px)`;
   });
+  window.countHour = setInterval(countingHour, 60000);
+  window.countMinute = setInterval(countingMinute, 1000);
+}
+function countingHour() {
+  countHourNum++;
+  if (countHourNum == timerNumValue[0]) {
+    console.log("Done...!");
+  } else {
+    timerHours.forEach(El => {
+      El.style.transform = `translateY(-${
+        60 * 40 - (60 - timerNumValue[0]) * 40 - 40 * countHourNum
+      }px)`;
+    });
+  }
+}
+function countingMinute() {
+  if (countHourNum == timerNumValue[0]) {
+    console.log("Done...!");
+  } else if (countMinuteNum == timerNumValue[1]) {
+    timerNumValue[1] = 60;
+    countMinuteNum = 0;
+  } else {
+    countMinuteNum++;
+    timerMinutes.forEach(El => {
+      El.style.transform = `translateY(-${
+        60 * 40 - (60 - timerNumValue[1]) * 40 - 40 * countMinuteNum
+      }px)`;
+    });
+  }
+  console.log(countMinuteNum);
 }
 function timerStoping() {
   timerStart.style.display = "flex";
   timerStop.style.display = "none";
+
+  clearInterval(window.countHour);
+  clearInterval(window.countMinute);
 }
 function timerEnding() {
   timerNums.forEach(El => {
@@ -45,12 +84,16 @@ function timerEnding() {
   });
   timerStart.style.display = "flex";
   timerStop.style.display = "none";
+
+  clearInterval(window.countHour);
+  clearInterval(window.countMinute);
+  countHourNum = 0;
+  countMinuteNum = 0;
 }
 
 timerNums.forEach(input => {
   input.addEventListener("input", inputMaxlength);
 });
-
 timerStart.addEventListener("click", timerStarting);
 timerStop.addEventListener("click", timerStoping);
 timerEnd.addEventListener("click", timerEnding);
