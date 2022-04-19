b5e2d4427a787fd13ce17f1ddf47e393b375ff28
chan-kim <chan.ckeem@gmail.com>
2022-04-19 19:30:19 +0900

    finish pomodoro mechanism

--- public/javascripts/pomodoro.js
+++ public/javascripts/pomodoro.js
@@ -9,15 +9,20 @@ const iconIntroClose = document.querySelectorAll(".icon-intro-closeJS");
 const loadingLeft = document.querySelector("#loadingLeftJS");
 const loadingRight = document.querySelector("#loadingRightJS");
 const CDCircle = document.querySelector("#CDCircleJS");
+const timeCondition = document.querySelector("#inside-CDCircle-timeJS");
 
 let page = 1;
 
 const INTROTEXTS = [
   "뽀모도로가 처음이신가요?",
+  "괜찮아요! 지금부터 설명해드릴게요",
   "뽀모도로란 루틴을 생성해주는 타이머에요!",
-  "지금부터 시작해볼까요?",
-  "시간을 입력해 주세요!",
+  "일할 시간과 쉴 시간을 설정하면",
+  "그때 그때 알람을 울려줍니다!",
+  "그럼 시작해볼까요?",
 ];
+// 수정할 때 html for문도 같이 수정해줄 것
+
 for (i in INTROTEXTS) {
   introText[i].innerHTML = INTROTEXTS[i];
 }
@@ -47,44 +52,69 @@ function startingPomodoro() {
   const inputValues = [];
 
   inputs.forEach(input => {
-    inputValues.push(input.value);
+    inputValues.push(Number(input.value));
   });
 
-  const WorkCDDuration = inputValues[0] * 3600 + inputValues[1] * 60;
-  const WorkCDRotate = (inputValues[0] * 60 + inputValues[1]) * 360;
+  const workCDDuration = inputValues[0] * 3600 + inputValues[1] * 60;
+  const workCDRotate = (inputValues[0] * 60 + inputValues[1]) * 360;
+  const PomodoroCDDuration = inputValues[2] * 3600 + inputValues[3] * 60;
+  const PomodoroCDRotate = (inputValues[2] * 60 + inputValues[3]) * 360;
 
-  pomodoroWork(WorkCDDuration, WorkCDRotate);
-  setTimeout(pomodoroBreak, WorkCDDuration * 1000);
+  pomodoroWork(workCDDuration, workCDRotate);
+
+  setTimeout(
+    pomodoroBreak,
+    workCDDuration * 1000,
+    PomodoroCDDuration,
+    PomodoroCDRotate
+  );
 }
-function pomodoroWork(WorkCDDuration, WorkCDRotate) {
+function pomodoroWork(duration, rotate) {
   timerinit();
-  CDCircle.style.transform = `rotate(${WorkCDRotate}deg)`;
-  CDCircle.style.transitionDuration = `${WorkCDDuration}s`;
+  timeCondition.innerHTML = "Work";
 
-  loadingLeft.style.animationName = "showPercent__left";
-  loadingRight.style.animationName = "showPercent__right";
+  CDCircle.style.transform = `rotate(${rotate}deg)`;
+  CDCircle.style.transitionDuration = `${duration}s`;
+
+  loadingLeft.style.animationName = "showPercent__leftWork";
+  loadingRight.style.animationName = "showPercent__rightWork";
 
   loadingLeft.style.animationPlayState = "running";
   loadingRight.style.animationPlayState = "running";
 
-  loadingLeft.style.animationDuration = `${WorkCDDuration / 2}s`;
-  loadingLeft.style.animationDelay = `${WorkCDDuration / 2}s`;
-  loadingRight.style.animationDuration = `${WorkCDDuration / 2}s`;
+  loadingLeft.style.animationDuration = `${duration / 2}s`;
+  loadingLeft.style.animationDelay = `${duration / 2}s`;
+  loadingRight.style.animationDuration = `${duration / 2}s`;
 }
-function pomodoroBreak() {
+function pomodoroBreak(duration, rotate) {
   timerinit();
-  alert("a");
+  timeCondition.innerHTML = "Break";
+
+  CDCircle.style.transform = `rotate(${rotate}deg)`;
+  CDCircle.style.transitionDuration = `${duration}s`;
+
+  loadingLeft.style.transform = "rotate(180deg)";
+  loadingRight.style.transform = "rotate(180deg)";
+
+  loadingLeft.style.animationName = "showPercent__leftBreak";
+  loadingRight.style.animationName = "showPercent__rightBreak";
+
+  loadingLeft.style.animationPlayState = "running";
+  loadingRight.style.animationPlayState = "running";
+
+  loadingLeft.style.animationDuration = `${duration / 2}s`;
+  loadingLeft.style.animationDelay = `${duration / 2}s`;
+  loadingRight.style.animationDuration = `${duration / 2}s`;
+
+  setTimeout(startingPomodoro, 12 * 1000);
 }
 function timerinit() {
   CDCircle.style.transform = "rotate(0deg)";
   CDCircle.style.transitionDuration = "0s";
 
-  loadingLeft.style.animationPlayState = "paused";
-  loadingRight.style.animationPlayState = "paused";
-
-  loadingLeft.style.animationName = "init";
-  loadingRight.style.animationName = "init";
-  alert("a");
+  loadingLeft.style.animationDuration = "";
+  loadingLeft.style.animationDelay = "";
+  loadingRight.style.animationDuration = "";
 }
 
 inputs.forEach(input => {
