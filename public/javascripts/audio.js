cd89844f31f3ab7a74194f867d1bf84d7800d935
chan-kim <chan.ckeem@gmail.com>
2022-05-12 17:02:06 +0900

    fix code for playing music

--- public/javascripts/audio.js
+++ public/javascripts/audio.js
@@ -1,12 +1,11 @@
 const btnPlay = document.querySelector("#btn-playJS");
 const iconPlay = document.querySelector("#icon-playJS");
 const musicRadioInput = document.querySelectorAll(".musicRadioInputJS");
+const musicNameJS = document.querySelector(".musicNameJS");
 
-let audio = new Audio("/audios/Hollow Knight - lofi chill mix.mp3");
+let audio = new Audio("/audios/" + musicNameJS.innerText + ".mp3");
 
-// 음악 고르게 할 때는 src바꾸면 될 듯
-//
-
+musicRadioInput[0].checked = true;
 audio.loop = false; // 반복재생하지 않음
 audio.volume = 1;
 console.log(audio);
@@ -15,22 +14,23 @@ function playingMusic() {
   if (iconPlay.className === "material-icons icon-pause") {
     iconPlay.innerHTML = "&#xe034;";
     audio.play();
+    iconPlay.classList.add("icon-play");
 
     console.log("play");
   } else {
-    iconPlay.innerHTML = "&#xe037;";
-    audio.pause();
-
-    console.log("pause");
+    pauseMusic();
   }
-  iconPlay.classList.toggle("icon-play");
 }
 
-function changingMusic() {
+function pauseMusic() {
   audio.pause();
   console.log("pause");
   iconPlay.innerHTML = "&#xe037;";
   iconPlay.classList.remove("icon-play");
+}
+
+function changingMusic() {
+  pauseMusic();
 
   const musicName = this.id;
   audio = new Audio("/audios/" + musicName + ".mp3");
