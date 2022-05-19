c382c188bcb860cbf3f75855b9a892f0e5c74018
chan-kim <chan.ckeem@gmail.com>
2022-05-20 01:05:25 +0900

    build audio controler

--- public/javascripts/audio.js
+++ public/javascripts/audio.js
@@ -5,26 +5,40 @@ const iconBack = document.querySelector("#icon-backJS");
 const musicRadioInput = document.querySelectorAll(".musicRadioInputJS");
 const musicNameJS = document.querySelectorAll(".musicNameJS");
 const musicEndPoint = document.querySelector("#musicEndPointJS");
+const minuteLineFill = document.querySelector("#minuteLineFillJS");
 
-let audio = new Audio("/audios/" + musicNameJS[0].innerText + ".mp3");
+let audio = new Audio("/audios/" + musicNameJS[1].innerText + ".mp3");
+let minuteFcCount = 1;
+let interval;
+let audioDuration;
 
 musicRadioInput[0].checked = true;
 audio.loop = false; // 반복재생하지 않음
 audio.volume = 1;
 
-function showAudioDuration(audio) {
-  console.dir(audio);
-  const duration = audio.target.duration;
-  const CalcDuration =
-    String(duration).substr(0, String(duration).indexOf(".")) * 1000;
-  const durationRefined = moment.utc(CalcDuration).format("HH:mm:ss");
-  musicEndPoint.innerText = durationRefined;
+function showAudioDuration(audioFile) {
+  audio.addEventListener("loadedmetadata", () => {
+    let duration = null;
+    if (audioFile.target === undefined) {
+      duration = audioFile.duration;
+    } else {
+      duration = audioFile.target.duration;
+    }
+    const CalcDuration = String(duration).substr(
+      0,
+      String(duration).indexOf(".")
+    );
+    audioDuration = CalcDuration;
+    const durationRefined = moment.utc(CalcDuration * 1000).format("HH:mm:ss");
+    musicEndPoint.innerText = durationRefined;
+  });
 }
 function playingMusic() {
   if (iconPlay.className === "material-icons icon-pause") {
     iconPlay.innerHTML = "&#xe034;";
     audio.play();
     iconPlay.classList.add("icon-play");
+    interval = setInterval(minuteLineFilling, 1000);
 
     console.log("play");
   } else {
@@ -38,14 +52,26 @@ function pauseMusic() {
   iconPlay.classList.remove("icon-play");
 }
 function changingMusic() {
-  console.log(audio);
   pauseMusic();
 
   const musicName = this.id;
   audio = new Audio("/audios/" + musicName + ".mp3");
   audio.load();
+  console.dir(audio);
+  showAudioDuration(audio);
   playingMusic();
-  console.log(audio);
+}
+let i = 1;
+function minuteLineFilling() {
+  console.log(i);
+  const minuteLineGo = 100 / audioDuration;
+  if (100 === minuteLineGo * i) {
+    alert("music done");
+  } else {
+    console.log(minuteLineFill.style.width);
+    minuteLineFill.style.width = `${minuteLineGo * i}%`;
+    i++;
+  }
 }
 function skipingMusic() {
   audio.pause();
@@ -53,11 +79,11 @@ function skipingMusic() {
 }
 function backingMusic() {}
 
+showAudioDuration(audio);
+
 btnPlay.addEventListener("click", playingMusic);
 musicRadioInput.forEach(input => {
   input.addEventListener("click", changingMusic);
 });
 iconSkip.addEventListener("click", skipingMusic);
 iconBack.addEventListener("click", backingMusic);
-audio.addEventListener("loadeddata", showAudioDuration, audio);
-audio.addEventListener("click", showAudioDuration, audio);
