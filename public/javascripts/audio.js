231f510a25187262227b33268607d63c4a9b25f9
chan-kim <chan.ckeem@gmail.com>
2022-05-19 15:55:47 +0900

    get musicEndPoint But It can't change

--- public/javascripts/audio.js
+++ public/javascripts/audio.js
@@ -4,6 +4,7 @@ const iconSkip = document.querySelector("#icon-skipJS");
 const iconBack = document.querySelector("#icon-backJS");
 const musicRadioInput = document.querySelectorAll(".musicRadioInputJS");
 const musicNameJS = document.querySelectorAll(".musicNameJS");
+const musicEndPoint = document.querySelector("#musicEndPointJS");
 
 let audio = new Audio("/audios/" + musicNameJS[0].innerText + ".mp3");
 
@@ -11,6 +12,14 @@ musicRadioInput[0].checked = true;
 audio.loop = false; // 반복재생하지 않음
 audio.volume = 1;
 
+function showAudioDuration(audio) {
+  console.dir(audio);
+  const duration = audio.target.duration;
+  const CalcDuration =
+    String(duration).substr(0, String(duration).indexOf(".")) * 1000;
+  const durationRefined = moment.utc(CalcDuration).format("HH:mm:ss");
+  musicEndPoint.innerText = durationRefined;
+}
 function playingMusic() {
   if (iconPlay.className === "material-icons icon-pause") {
     iconPlay.innerHTML = "&#xe034;";
@@ -29,11 +38,14 @@ function pauseMusic() {
   iconPlay.classList.remove("icon-play");
 }
 function changingMusic() {
+  console.log(audio);
   pauseMusic();
 
   const musicName = this.id;
   audio = new Audio("/audios/" + musicName + ".mp3");
+  audio.load();
   playingMusic();
+  console.log(audio);
 }
 function skipingMusic() {
   audio.pause();
@@ -47,3 +59,5 @@ musicRadioInput.forEach(input => {
 });
 iconSkip.addEventListener("click", skipingMusic);
 iconBack.addEventListener("click", backingMusic);
+audio.addEventListener("loadeddata", showAudioDuration, audio);
+audio.addEventListener("click", showAudioDuration, audio);
