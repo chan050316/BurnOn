de98b74cb1a6d94d41b8d06dcb2314660af7ea02
chan-kim <chan.ckeem@gmail.com>
2022-06-09 15:59:14 +0900

    create get current time of custom option

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,6 +1,7 @@
 const titleCover = document.querySelector("#titleCoverJS");
 const titleBoxTitle = document.querySelectorAll(".titleBox_titleJS");
 const customForm = document.querySelector("#customForm");
+const currentTime = document.querySelector("#currentTimeJS");
 
 const cookie = document.cookie;
 const customOption = cookie.substr(cookie.indexOf("=") + 1, cookie.length);
@@ -18,4 +19,19 @@ if (customOption) {
 //   titleCover.style
 // }
 
+function getTime() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  // option-time
+  currentTime.innerText = `${hours}:${minutes}:${seconds}`;
+
+  // option-showSchedule
+  console.dir(date);
+}
+
 // titleCover.addEventListener("onload", siteStartAnimation);
+getTime();
+setInterval(getTime, 1000);
