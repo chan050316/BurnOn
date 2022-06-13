afcd6b1321e75370bcf5e1b8f447479423765d74
chan-kim <chan.ckeem@gmail.com>
2022-06-14 03:12:59 +0900

    change sequelize setting2

--- models/index.js
+++ models/index.js
@@ -4,10 +4,11 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+const env = "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
+console.log(config);
 let sequelize;
 sequelize = new Sequelize(
   config.database,
